def4d3ca07948cd5fa76416793288b62e2814e78
aalemanyz67 <albertaleman67@gmail.com>
2023-10-12 18:49:03 -0500

    updated javascript

--- assets/script.js
+++ assets/script.js
@@ -18,13 +18,14 @@ var getCityCoordinates = () => {
             var lat = data[0].lat
             var lon = data[0].lon
             getCurrentWeather(lat, lon,)
+            getWeatherForecast(lat, lon)
 
         })
          
 }
 
 var getCurrentWeather = (lat, lon) => {
-    var currentUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey 
+    var currentUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey + "&units=metric"; 
     fetch(currentUrl)
         .then(res => res.json())
         .then(data => {
@@ -45,34 +46,88 @@ var getCurrentWeather = (lat, lon) => {
             windEl.textContent = "Wind: " + wind + "mph"
             humidityEl.textContent = "Humidity: " + humidity
 
-            if(data.weather[0].main == "clouds"){
+            if(data.weather[0].main == "Clouds"){
                 weatherIcon.src ="images/clouds.png";
 
             }
-            else if(data.weather[0].main == "clear"){
+            else if(data.weather[0].main == "Clear"){
                 weatherIcon.src ="images/clear.png";
             
             }
-            else if(data.weather[0].main == "drizzle"){
+            else if(data.weather[0].main == "Drizzle"){
                 weatherIcon.src ="images/drizzle.png";
             }
-            else if(data.weather[0].main == "rainy"){
+            else if(data.weather[0].main == "Rainy"){
                 weatherIcon.src ="images/rainy.png";
             }
-            else if(data.weather[0].main == "sunny"){
+            else if(data.weather[0].main == "Sunny"){
                 weatherIcon.src ="images/sunny.png";
             }
-            else if(data.weather[0].main == "thunderstorm"){
+            else if(data.weather[0].main == "Thunderstorm"){
                 weatherIcon.src ="images/thunderstorm.png";
             }
 
 
         
         })
-}
-searchButton.addEventListener("click", getCityCoordinates);
 
-var cityInput = JSON.parse(localStorage.getItem("city-input"));
+       
+}
+
+var getWeatherForecast = (lat, lon) => {
+    var currentUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey + "&units=metric";
+    fetch(currentUrl)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+
+            for(var i = 0; i < 5; i ++){
+             var day = data.list[i * 8] 
+             day.main.temp  
+             var dayEl = document.querySelector(".card-" +(i+1));
+             dayEl.children[2].textContent = "Temperature: " + day.main.temp;
+             dayEl.children[3].textContent = "Wind: " + day.wind.speed + " mph";
+             dayEl.children[4].textContent = "Humidity: " + day.main.humidity;
+             dayEl.children[0].textContent = "Date: " + day.dt_txt.split(" ")[0];
+
+             if(day.weather[0].main == "Clouds"){
+                dayEl.children[1].src ="images/clouds.png";
+
+            }
+            else if(day.weather[0].main == "Clear"){
+                dayEl.children[1].src ="images/clear.png";
+            
+            }
+            else if(day.weather[0].main == "Drizzle"){
+                dayEl.children[1].src ="images/drizzle.png";
+            }
+            else if(day.weather[0].main == "Rainy"){
+                dayEl.children[1].src ="images/rainy.png";
+            }
+            else if(day.weather[0].main == "Sunny"){
+                dayEl.children[1].src ="images/sunny.png";
+            }
+            else if(day.weather[0].main == "Thunderstorm"){
+                dayEl.children[1].src ="images/thunderstorm.png";
+            }
+
+
+            }
+
+
+        })
+    }    
+
+
+
+
+
+
+
+
+
+
+    //var cityInput = JSON.parse(localStorage.getItem("city-input"));
 
 
 
@@ -80,3 +135,4 @@ var cityInput = JSON.parse(localStorage.getItem("city-input"));
 //Function to get weather forcast
 //Append data bellow search bar for city history
 
+searchButton.addEventListener("click", getCityCoordinates);
